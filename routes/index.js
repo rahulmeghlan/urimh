8f548c8ddb8ab1784927d8b59eb08f42daf081c0
Rahul Meghlan <rmeghlan@TS-0200-RMEGHLAN.local>
2017-04-17 00:24:41 +0530

    added a function for 1 year timeline

--- routes/index.js
+++ routes/index.js
@@ -59,3 +59,15 @@ router.get('/', function (req, res, next) {
 
 
 module.exports = router;
+
+/*
+todo: try to use this function instead when finalizing 1 year timeline
+function runAtDate(date, func) {
+    var now = (new Date()).getTime();
+    var then = date.getTime();
+    var diff = Math.max((then - now), 0);
+    if (diff > 0x7FFFFFFF) //setTimeout limit is MAX_INT32=(2^31-1)
+        setTimeout(function() {runAtDate(date, func);}, 0x7FFFFFFF);
+    else
+        setTimeout(func, diff);
+}*/
